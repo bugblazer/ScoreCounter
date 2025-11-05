3c8449fbe69b8f52dccd0280c982b010b3c89806
bugblazer <daregamer2004@gmail.com>
2025-11-05 20:21:13 +0500

    Completed the scoreboard project

---
+++ index.js
@@ -0,0 +1,36 @@
+let homeScore = 0;
+let guestScore = 0;
+
+function homeScoreBoard(id) {
+   if (id === "homeOne") {
+        homeScore += 1;
+        document.getElementById("home-score").innerText = homeScore;
+   }
+   
+   else if(id === "homeTwo") {
+        homeScore += 2;
+        document.getElementById("home-score").innerText = homeScore;
+   }
+   
+   else {
+        homeScore += 3;
+        document.getElementById("home-score").innerText = homeScore;
+   }
+}
+
+function guestScoreBoard(id) {
+    if (id === "guestOne") {
+        guestScore += 1;
+        document.getElementById("guest-score").innerText = guestScore;
+    }
+    
+    else if(id === "guestTwo") {
+        guestScore += 2;
+        document.getElementById("guest-score").innerText = guestScore;
+    }
+    
+    else {
+        guestScore += 3;
+        document.getElementById("guest-score").innerText = guestScore;
+    }
+}
