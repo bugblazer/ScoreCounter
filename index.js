e6466dcbc3451f10cc92938105d98d15b674f312
bugblazer <daregamer2004@gmail.com>
2025-11-05 23:41:49 +0500

    Changed all occurrences of .innterText to .textContent

--- index.js
+++ index.js
@@ -4,38 +4,40 @@ let guestScore = 0;
 function homeScoreBoard(id) {
    if (id === "homeOne") {
         homeScore += 1;
-        document.getElementById("home-score").innerText = homeScore;
+        document.getElementById("home-score").textContent = homeScore;
    }
    
    else if(id === "homeTwo") {
         homeScore += 2;
-        document.getElementById("home-score").innerText = homeScore;
+        document.getElementById("home-score").textContent = homeScore;
    }
    
    else {
         homeScore += 3;
-        document.getElementById("home-score").innerText = homeScore;
+        document.getElementById("home-score").textContent = homeScore;
    }
 }
 
 function guestScoreBoard(id) {
     if (id === "guestOne") {
         guestScore += 1;
-        document.getElementById("guest-score").innerText = guestScore;
+        document.getElementById("guest-score").textContent = guestScore;
     }
     
     else if(id === "guestTwo") {
         guestScore += 2;
-        document.getElementById("guest-score").innerText = guestScore;
+        document.getElementById("guest-score").textContent = guestScore;
     }
     
     else {
         guestScore += 3;
-        document.getElementById("guest-score").innerText = guestScore;
+        document.getElementById("guest-score").textContent = guestScore;
     }
 }
 
 function reset() {
-    document.getElementById("home-score").innerText = 0;
-    document.getElementById("guest-score").innerText = 0;
+    homeScore = 0;
+    guestScore = 0;
+    document.getElementById("home-score").textContent = homeScore;
+    document.getElementById("guest-score").textContent = guestScore;
 }
